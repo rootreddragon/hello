6f221b528396f728ba06a78dec28c5e2c6c8c1a8
rootreddragon <mohibulmomenutsho@yahoo.com>
2018-09-04 18:52:27 +0300

    utshomomen

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
    cout<<"Hello, world!"<<endl;
-   cout<<"My name is Vasya!"<<endl;
-   cout <<"Hello from Di and DArt"<< endl;
+   cout<<"My name is utsho"<<endl;
+   cout <<"Hello "<< endl;
    cout <<"<3"<<endl;
    cout << "Fork!" << endl;
    return 0;
